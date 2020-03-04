4aebba5184ed3354c01437605498ae9323ef3e06
Josué Martel <josue.martel@landau.com>
2020-03-04 16:03:46 -0600

    Primer commit

---
+++ InfinityIdeas/InfinityIdeas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InfinityIdeas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
